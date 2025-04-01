db07cbc802a40af8cd3e69dd3b4c23bfdf292b8c
Saltaformajo <i8ehkvien@mozmail.com>
2025-04-01 11:58:41 +0100

    fix: make sure function will be declared only once
    production.ERROR: Cannot redeclare registerKeyRoute

--- routes/web.php
+++ routes/web.php
@@ -7,44 +7,37 @@ use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 use Ymigval\LaravelIndexnow\IndexNowApiKeyManager;
 use Ymigval\LaravelIndexnow\LogManager;
 
+if (!function_exists('registerIndexNowKeyRoute')) {
+    /**
+     * Generates the path for the IndexNow API key route.
+     *
+     * @param string $apiKey
+     * @return string
+     */
+    function generateIndexNowKeyRoutePath(string $apiKey): string
+    {
+        return Str::of('/')->append($apiKey)->append('.txt')->toString();
+    }
+    /**
+     * Registers the IndexNow API key route.
+     *
+     * @param string $apiKey
+     */
+    function registerIndexNowKeyRoute(string $apiKey): void
+    {
+        Route::any(generateIndexNowKeyRoutePath($apiKey), function () use ($apiKey) {
+            return new Response($apiKey, ResponseAlias::HTTP_OK, [
+                'Content-Type' => 'text/plain; charset=utf-8',
+            ]);
+        })->name('my_key_index_now');
+    }
 
-/**
- * Content-Type constant for UTF-8 plain text responses.
- */
-const TEXT_PLAIN_UTF8 = 'text/plain; charset=utf-8';
-
-
-try {
-    $apiKey = IndexNowApiKeyManager::fetchOrGenerate();
-    registerKeyRoute($apiKey);
-} catch (Exception $e) {
-    LogManager::addMessage('Error registering IndexNow route: ' . $e->getMessage());
-}
-
-/**
- * Registers the IndexNow API key route.
- *
- * @param string $apiKey
- */
-function registerKeyRoute(string $apiKey): void
-{
-    Route::any(generateKeyRoutePath($apiKey), function () use ($apiKey) {
-        return new Response(
-            $apiKey,
-            ResponseAlias::HTTP_OK,
-            ['Content-Type' => TEXT_PLAIN_UTF8]
+    try {
+        $apiKey = IndexNowApiKeyManager::fetchOrGenerate();
+        registerIndexNowKeyRoute($apiKey);
+    } catch (Exception $e) {
+        LogManager::addMessage(
+            'Error registering IndexNow route: ' . $e->getMessage()
         );
-    })->name('my_key_index_now');
+    }
 }
-
-/**
- * Generates the path for the IndexNow API key route.
- *
- * @param string $apiKey
- * @return string
- */
-function generateKeyRoutePath(string $apiKey): string
-{
-    return Str::of('/')->append($apiKey)->append('.txt')->toString();
-}
-
